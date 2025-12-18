5750fd599d8676f4f2564b5a8134414a28878931
David Kiknavelidze <davidkiknavelidze9@gmail.com>
2025-12-18 23:21:25 +0400

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,9 @@
+// export default function Footer() {}
+import styles from "./Footer.module.css";
+
+
+const Footer = () => {
+  return <div className={styles.footer}>this is footer!</div>;
+};
+
+export default Footer;
